76680cdedfcdaa8cabae7dc29c592bf18b116967
hansomesong <qpsong@gmail.com>
2016-08-12 05:19:52 +0200

    Modify script log_processor.py. Now it can support iterate a series of csv log file

--- log_processor.py
+++ log_processor.py
@@ -7,18 +7,23 @@ import numpy as np
 import scipy.stats as st
 import os
 import pandas as pd
+import glob
+
 LOG_DIR = 'logs'
-f_name = "simd=5000_N=500_threshold=3.0_l=1_m=1_backoff=150_start=0.44_end=0.44_simstep=0.02_032136.csv"
+SUB_DIR = 'fading_shadowing'
 
-csv_file = os.path.join(
-    LOG_DIR,
-    f_name
-)
+all_logs = glob.glob(os.path.join(LOG_DIR, SUB_DIR, "*.csv"))
 
-csv_df = pd.read_csv(csv_file, sep=',', header=None)
+f_name = "simd=5000_N=500_threshold=3.0_l=1_m=1_backoff=150_start=0.8_end=0.8_simstep=0.02_20160812040142.csv"
 
-plr = csv_df.values[:, -2]
+for csv_file in all_logs:
 
-ci =st.t.interval(0.95, len(plr)-1, loc=np.mean(plr), scale=st.sem(plr))
+    csv_df = pd.read_csv(csv_file, sep=',', header=None)
 
-print np.mean(plr), ci
+    plr = csv_df.values[:, -2]
+
+    alpha = csv_df.values[:, -1][0]
+
+    ci =st.t.interval(0.95, len(plr)-1, loc=np.mean(plr), scale=st.sem(plr))
+
+    print alpha, np.mean(plr), ci
