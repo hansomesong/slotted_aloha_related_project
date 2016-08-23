c3647cb45cee192b0e25297ac1d4a11cafc52230
hansomesong <qpsong@gmail.com>
2016-08-23 00:33:17 -0300

    Modify the script new_SINR_simplifed_simulator.py and the way it read the simulation configuration file. It seems to work well for case of 0dB (alpha from 0.6 to 0.7). Commit for the verfication in another Mac.

--- log_processor.py
+++ log_processor.py
@@ -17,9 +17,11 @@ SUBSUB_DIR = "backoff_50"
 SUBSUBSUB_DIR = "l_1_m_1_sigma_s_1"
 
 all_logs = glob.glob(os.path.join(LOG_DIR, SUB_DIR, CASE_DIR, SUBSUB_DIR, SUBSUBSUB_DIR, "*.csv"))
-# all_logs = [os.path.join(LOG_DIR,
-#                  "simd=5000_N=500_threshold=3.0_l=1_m=1_backoff=50_start=0.4_end=0.4_simstep=0.02_sigmas=1.0_20160813014646.csv")
-# ]
+all_logs = [
+    os.path.join(
+        LOG_DIR,
+        "simd=10000_N=300_threshold=0.0dB_l=1_m=1_backoff=50_alpha=0.64_mufading_1.0_mushadowing=0_sigmashadowing=1.0_tmp=20160823000025.csv")
+]
 for csv_file in all_logs:
 
     csv_df = pd.read_csv(csv_file, sep=',', header=None)
--- new_SINR_simplifed_simulator.py
+++ new_SINR_simplifed_simulator.py
@@ -52,15 +52,6 @@ def run_simulation(alpha, max_trans, device_nb, threshold, l, m, backoff, sim_du
         ipc_way: referst to imperfect power control way. If set as "DIFF", each retransmission power error may be different.
         If set as "SAME", each retransmission power error is identical.
     '''
-    # LM = []
-    # if ipc_way == "DIFF":
-    #     LM = [1.0*l**(k)*m**(max_trans-k-1) for k in range(max_trans)]
-    # elif ipc_way == "SAME":
-    #     LM = [(1.0*l/m)**k for k in range(max_trans)]
-    #     IPC_LEVEL = np.random.lognormal(mu_ipc, sigma_shadowing, device_nb)
-    # else:
-    #     print "Wrong configuration for Imperfect power control manner"
-    #     exit()
     BETA = np.log(10)/10.0
     LM = [1.0*l**(k)*m**(max_trans-k-1) for k in range(max_trans)]
     start_t = int(time())
@@ -68,31 +59,34 @@ def run_simulation(alpha, max_trans, device_nb, threshold, l, m, backoff, sim_du
     np.random.seed(seed)
     sim_history = np.zeros((sim_duration, device_nb), dtype=np.int)
     # shadowings = np.random.lognormal(BETA*mu_shadowing, BETA*sigma_shadowing, device_nb)
-
     for slot in range(sim_duration-1):
         # First generate new packets.
         # Which device has packets to transmit?
         # In the following, k refers to the k th transmision instead of retransmission
         sim_history[slot] = np.array([bernoulli.rvs(alpha/device_nb) if k == 0 else k for k in sim_history[slot]])
-
         # With which transmit power they can sue?
         power_levels = np.array([LM[k-1] if k != 0 else k*1.0 for k in sim_history[slot]])
-        shadowings = np.random.lognormal(BETA*mu_shadowing, BETA*sigma_shadowing, device_nb)
-        fadings = np.random.exponential(scale=mu_fading, size=device_nb)
-        power_levels *= fadings*shadowings
-        # print power_levels
-        # power_levels *= shadowings
-        # nb_pkts = sum([1 for k in sim_history[slot] if k != 0])
+        # 如果 shadowing 的方差不是0，那么生成一系列 log-normal 随机数，否则生成同等长度的1
+        if sigma_shadowing > 0:
+            shadowings = np.random.lognormal(BETA*mu_shadowing, BETA*sigma_shadowing, device_nb)
+        else:
+            shadowings = np.ones(device_nb)
 
+        # 对于满足指数分布的 fading effect, 也是同理
+        if mu_fading > 0:
+            fadings = np.random.exponential(scale=mu_fading, size=device_nb)
+        else:
+            fadings = np.ones(device_nb)
+
+        # 计算考虑诸多因素之后的，接受功率
+        power_levels *= fadings*shadowings
+        total_p = sum(power_levels)
         # 我们采用 SINR门限值*干扰值 和 接收功率 比较的方式，加速仿真的执行
         # (计算实际接收信噪比的思路会不可避免地考虑信道中只有一个传输的情况，导致程序的执行效率低下)
-        total_p = sum(power_levels)
-
         curr_trans_results = [
             (10**(0.1*threshold)) * (total_p - power_levels[device_id]) > power_levels[device_id]
             if k != 0 else False for device_id, k in enumerate(sim_history[slot])
         ]
-
         # print curr_trans_results
         for device_id, curr_trans_result in enumerate(curr_trans_results):
             # 如果 SINR门限值*干扰值 大于 接收功率，则需要对这个包执行重传操作
@@ -152,11 +146,6 @@ def run_simulation(alpha, max_trans, device_nb, threshold, l, m, backoff, sim_du
 
         # Second, generate packet and schedule the transmission in the next slot for each device
 
-    # print "History:", sim_history[warm_t:sim_duration, ::].reshape(1, device_nb*(sim_duration-warm_t))
-    # statistics = np.bincount(
-    #     sim_history[warm_t:sim_duration, ::].reshape(1, device_nb*(sim_duration-warm_t))[0]
-    # )[1:]
-
     # 统计第 i 次传输出现的次数，并据此计算至少需要传输 i 次的频数，如果实验足够长，频数将趋近于概率
     statistics = itemfreq(sim_history[warm_t:sim_duration, ::].reshape(1, device_nb*(sim_duration-warm_t))[0])[1:]
 
@@ -169,52 +158,44 @@ def run_simulation(alpha, max_trans, device_nb, threshold, l, m, backoff, sim_du
         satisfied_trans = sum([element[1] for element in statistics if element[0] >= ith_trans])
         vector_p[ith_trans - 1] = satisfied_trans*1.0/total_transmission
 
-
     # Initialize the return probability vector with 0, the length of this vector is 1+MAX_ALLOWED_RETRANSMISSION_NB
     # vector_p = [sum(statistics[i:])*1.0/sum(statistics) for i in range(len(statistics))]
     end_t = int(time())
     time_elapsed = float(end_t-start_t)/60.0
-
-    pprint.pprint(sim_history)
-    print "Execution time", int(time_elapsed), "for this task", alpha, "with seed ", seed, "Result:", statistics, vector_p
+    print "Time:", int(time_elapsed), "Alpha:", alpha, "Seed:", seed, "Result:", [e[1] for e in statistics], vector_p
     return alpha, list(statistics), vector_p
 
-def main(com_config_f, logs_directory):
+def main(sim_config_dict, logs_directory):
     '''
-    :param com_config_f: 公共配置文件，存储着通用的仿真参数。
+    :param com_config_f: 公共配置字典，存储着通用的仿真参数。
     :param logs_directory:
     :return: nothing
     '''
     #must use Manager queue here, or will not work
     # 注意：一定要使用 Manager queue，否则无法工作，至于原因，我不晓得
-    with open(com_config_f) as json_file:
-        json_config = json.load(json_file)
 
-    SIM_DURATION = json_config['SIM_DURATION']
-    DEVICE_NB = json_config['N']
-    SIM_STEP = json_config['sim_step']
-    BACKOFF = json_config["BACKOFF"]
-    THERSHOLD = json_config['THRESLD']
-    L = json_config['l']
-    M = json_config['m']
-    MAX_TRANS = json_config['MAX_TRANS']
-    ALPHA_START = json_config['alpha_start']
-    ALPHA_END = json_config['alpha_end']
-    WARM_T = json_config['WARM_UP']
-    ALPHA_INTERVAL = [ALPHA_START + i*SIM_STEP for i in range(int((ALPHA_END-ALPHA_START)/SIM_STEP)+1)]
-    MU_FADING = json_config['MU_FADING']
-    MU_SHADOWING = json_config['MU_SHADOWING']
-    SIGMA_SHADOWING = json_config['SIGMA_SHADOWING']
+    SIM_DURATION = sim_config_dict['SIM_DURATION']
+    DEVICE_NB = sim_config_dict["DEVICE_NB"]
+    BACKOFF = sim_config_dict["BACKOFF"]
+    THERSHOLD = sim_config_dict['THRESLD']
+    L = sim_config_dict['L']
+    M = sim_config_dict['M']
+    MAX_TRANS = sim_config_dict['MAX_TRANS']
+    ALPHA = sim_config_dict['ALPHA']
+    WARM_T = sim_config_dict['WARM_UP']
+    MU_FADING = sim_config_dict['MU_FADING']
+    MU_SHADOWING = sim_config_dict['MU_SHADOWING']
+    SIGMA_SHADOWING = sim_config_dict['SIGMA_SHADOWING']
+    # 针对每个 alpha 值，仿真重复次数
+    SIM_REPEAT_NB = sim_config_dict['SIM_REPEAT_NB']
 
-    n = 10
-    if len(ALPHA_INTERVAL) == 1:
-        ALPHA_INTERVAL = [ALPHA_START for i in range(n)]
+    ALPHAS = [ALPHA for i in range(SIM_REPEAT_NB)]
 
     # 将仿真结果存储在 sim_result_f 指向的文件中
     sim_result_f = os.path.join(
         logs_directory,
-        "simd={0}_N={1}_threshold={2}_l={3}_m={4}_backoff={5}_start={6}_end={7}_simstep={8}_sigmas={9}_{10}.csv".format(
-            SIM_DURATION, DEVICE_NB, THERSHOLD, L, M, BACKOFF, ALPHA_START, ALPHA_END, SIM_STEP, SIGMA_SHADOWING, strftime("%Y%m%d%H%M%S")
+        "simd={0}_N={1}_threshold={2}dB_l={3}_m={4}_backoff={5}_alpha={6}_mufading_{7}_mushadowing={8}_sigmashadowing={9}_tmp={10}.csv".format(
+            SIM_DURATION, DEVICE_NB, THERSHOLD, L, M, BACKOFF, ALPHA, MU_FADING, MU_SHADOWING, SIGMA_SHADOWING, strftime("%Y%m%d%H%M%S")
         )
     )
 
@@ -223,13 +204,14 @@ def main(com_config_f, logs_directory):
     pool = mp.Pool(mp.cpu_count()+3)
 
     f_handler = open(sim_result_f, 'w')
-    #put listener to work first
+
+    # put listener to work first
     watcher = pool.apply_async(listener, (sim_result_f, q,))
 
     #fire off workers
     jobs = []
 
-    for alpha in ALPHA_INTERVAL:
+    for alpha in ALPHAS:
         job = pool.apply_async(
             worker,
             (alpha, MAX_TRANS, DEVICE_NB, THERSHOLD, L, M, BACKOFF, SIM_DURATION, WARM_T, MU_FADING, MU_SHADOWING, SIGMA_SHADOWING, q)
@@ -250,24 +232,58 @@ if __name__ == "__main__":
 
     start_t = int(time())
 
-    all_logs = glob.glob(os.path.join('sim_configs', 'fading_shadowing', "*.json"))
     logs_directory = 'logs'
-    print all_logs
+    # The simulation result will be logged into files of type CSV, in folder logs.
+    # First check the existence of this folder and creat it if necessary.
+    if not os.path.exists(logs_directory):
+        os.makedirs(logs_directory)
 
-    for config_f in all_logs:
+    sim_config_f = os.path.join('sim_configs', 'fading_shadowing', 'case_K=5_alpha=1p00_l=1_m=1_threshold=0dB.json')
+    print "Now do simulation with configuration file: ", sim_config_f
 
-        # config_f = os.path.join('sim_configs', 'case_K=5_l=1_m=1_threshold=3dB.json')
-        # The simulation result will be logged into files of type CSV, in folder logs.
-        # First check the existence of this folder and creat it if necessary.
+    sim_config_dict = {}
 
+    with open(sim_config_f) as json_file:
+        json_config = json.load(json_file)
 
-        if not os.path.exists(logs_directory):
-            os.makedirs(logs_directory)
-        print "Now do simulation with configuration file: ", config_f
+    sim_config_dict["BACKOFF"] = json_config["BACKOFF"]
+    sim_config_dict['THRESLD'] = json_config['THRESLD']
+    sim_config_dict["L"] = json_config['L']
+    sim_config_dict["M"] = json_config['M']
+    sim_config_dict["MAX_TRANS"] = json_config['MAX_TRANS']
+    sim_config_dict["WARM_UP"] = json_config['WARM_UP']
+    sim_config_dict["MU_FADING"] = json_config['MU_FADING']
+    sim_config_dict["MU_SHADOWING"] = json_config['MU_SHADOWING']
+    sim_config_dict["SIGMA_SHADOWING"] = json_config['SIGMA_SHADOWING']
+    # 针对每个 alpha 值，仿真重复次数
+    sim_config_dict["SIM_REPEAT_NB"] = json_config['SIM_REPEAT_NB']
+    sim_config_dict["SIM_INCRE_STEP"] = json_config['SIM_INCRE_STEP']
 
-        main(config_f, logs_directory)
-        end_t = int(time())
-        time_elapsed = float(end_t - start_t)/60.0
+    ALPHA_START = json_config['ALPHA_START']
+    ALPHA_END = json_config['ALPHA_END']
+    SIM_INCRE_STEP = json_config['SIM_INCRE_STEP']
+    SIM_DURATION = json_config['SIM_DURATION']
+    DEVICE_NB = json_config['DEVICE_NB']
+    ALPHA_INTERVAL = np.arange(ALPHA_START, ALPHA_END, SIM_INCRE_STEP)
 
-        print "Total Execution time: ", time_elapsed
+    for order, ALPHA in enumerate(ALPHA_INTERVAL, 1):
+        # 我们一般从 packet loss rate 大致为 0.01 的 ALPHA 值开始，所以前四次仿真，我们把 simulation duration 设为 10000
+        # 仿真需要的设备数目因而也不需要很高 (因为 ALPHA 取值不高)
+        sim_config_dict["ALPHA"] = ALPHA
+        if order < 5:
+            sim_config_dict["DEVICE_NB"] = DEVICE_NB[0]
+            sim_config_dict["SIM_DURATION"] = SIM_DURATION[0]
+        elif order >= 5 and order < 16:
+            sim_config_dict["DEVICE_NB"] = DEVICE_NB[1]
+            sim_config_dict["SIM_DURATION"] = SIM_DURATION[1]
+        else:
+            sim_config_dict["DEVICE_NB"] = DEVICE_NB[2]
+            sim_config_dict["SIM_DURATION"] = SIM_DURATION[2]
+        print sim_config_dict
+        # 将填充好的仿真参数字典传递给 main(), 开启多进程下的仿真
+        main(sim_config_dict, logs_directory)
+
+    end_t = int(time())
+    time_elapsed = float(end_t - start_t)/60.0
+    print "Total Execution time: ", time_elapsed
 
