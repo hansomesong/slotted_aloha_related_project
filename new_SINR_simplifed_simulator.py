2f9ebcd293c9ca6553bc97f27a868e33f09dbd0b
hansomesong <qpsong@gmail.com>
2016-09-30 07:17:24 +0200

    A problem finally to be faced with: I don't know why for perfect case, 3dB, l=1, m=2. The simulation in current has a packet loss rate 0.25, while the simulation in branch about_SINR has 0.4 (virtually the same with analytical result). The difference between these two branches is: one I just process the slot with multiple packets, the other I treat them in the same way...

--- new_SINR_simplifed_simulator.py
+++ new_SINR_simplifed_simulator.py
@@ -56,6 +56,9 @@ def run_simulation(alpha, max_trans, binomial_p, threshold, l, m, backoff, sim_d
     # Generate the random number generator seed, make sure each process uses a different seed otherwise
     # the simulation result will be the same when multiple processes run the this method.
     seed = hash((start_t + os.getpid()*13)*0.0000001)
+    # seed = 2160133643
+    # seed = 1792749438
+    # seed = 2954458010
     np.random.seed(seed)
 
     BETA = np.log(10)/10.0
@@ -66,126 +69,131 @@ def run_simulation(alpha, max_trans, binomial_p, threshold, l, m, backoff, sim_d
     # The involved device number should be not less than 400.
     # The probability p in binomial distribution should less than 0.01 to assure that binomial distribution is close to poisson distribution
     device_nb = int(alpha/binomial_p)
-    # print "Possible Back off values:", BACK_OFFS
-    # For example, if max_trans = 5, that means device may have 5 different transmit power level
-    # if l=2, m=1, then
-    # LM = [1, 2, 4, 8, 16]
-    # Now Add one zero in the beginning of LM list, to facilitate the generation of
-    # Then each element in LM list represents the transmit power for the kth transmission.
-    # The 0th transmission transmit power is 0
-    LM = [1.0*np.power(l, k-1)*np.power(m, max_trans-k-2) if k != 0.0 else 0.0 for k in range(max_trans+1)]
-    normalized_sinr = []
 
-    transmissions_nb = []
-    sim_history = np.zeros((sim_duration, device_nb), dtype=np.int)
-    for slot in range(sim_duration-1):
+    # Now Add one zero in the beginning of LM list, to facilitate the generation of
+    # Then each element in LM list represents the transmit power for the kth transmission (Not retransmission).
+    # For example, if max_trans = 5, l=2, m=1, then
+    # LM = [0, 1, 2, 4, 8, 16] The 0th transmission transmit power is of course 0
+    LM = [1.0*np.power(l, k-1)*np.power(m, max_trans-k-2) if k != 0.0 else 0.0 for k in range(max_trans+1)]
+
+    # sim_history now is 3-d array.
+    # The third dimension is used to represent the transmission index and received power levels history
+    sim_history = np.zeros((sim_duration, device_nb, max_trans+1), dtype=np.float)
+    for slot in range(sim_duration):
         # First generate new packets.
         # Which device has packets to transmit?
         # In the following, k refers to the k th transmision instead of retransmission
-        sim_history[slot] = np.array([bernoulli.rvs(binomial_p) if k == 0.0 else k for k in sim_history[slot]])
-        # With which transmit power they can sue?
-        power_levels = np.array([LM[k] for k in sim_history[slot]])
-
-        transmissions_nb.append(sum([1 for power_level in power_levels if power_level > 0.0]))
+        sim_history[slot, :, 0] = np.array(
+            [bernoulli.rvs(binomial_p) if k == 0.0 else k for k in sim_history[slot, :, 0]]
+        )
+        # With which receive power they can sue?
+        rec_power_levels = np.array([LM[int(k)] for k in sim_history[slot, :, 0]])
         # 如果 shadowing 的方差不是0，那么生成一系列 log-normal 随机数，否则生成同等长度的1
         if sigma_shadowing > 0:
             shadowings = np.random.lognormal(BETA*mu_shadowing, BETA*sigma_shadowing, device_nb)
         else:
             shadowings = np.ones(device_nb)
 
-        # 对于满足指数分布的 fading effect, 也是同理
         if mu_fading > 0:
             fadings = np.random.exponential(scale=mu_fading, size=device_nb)
         else:
             fadings = np.ones(device_nb)
 
-        # 计算考虑诸多因素之后的，接受功率
-        power_levels *= fadings*shadowings
+        # With impact of factors such as fading and power control error
+        rec_power_levels *= fadings*shadowings
 
-        total_p = sum(power_levels)
-        # print "Total Levels", total_p
+        # 将每个slot，每个设备消耗的能量，存进 energy_history. 我们假设整个slot都以恒定的功率传输数据(不知道这个假设效果如何)
+        for device_id, trans_index in enumerate(sim_history[slot, :, 0]):
+            sim_history[slot, device_id, trans_index] = rec_power_levels[device_id]
+
+        total_p = sum(rec_power_levels)
         # 我们采用 SINR门限值*干扰值 和 接收功率 比较的方式，加速仿真的执行
         # (计算实际接收信噪比的思路会不可避免地考虑信道中只有一个传输的情况，导致程序的执行效率低下)
-        # curr_trans_results = [
-        #     (10**(0.1*threshold)) * (total_p - power_levels[device_id]) > power_levels[device_id]
-        #     if k != 0 else False for device_id, k in enumerate(sim_history[slot])
-        # ]
         curr_trans_results = [
-            (total_p - power_levels[device_id])/power_levels[device_id] > 1.0/(10**(0.1*threshold))
-            if k != 0 else False for device_id, k in enumerate(sim_history[slot])
+            (total_p - rec_power_levels[device_id])/rec_power_levels[device_id] > 1.0/(10**(0.1*threshold))
+            if k != 0 else False for device_id, k in enumerate(sim_history[slot, :, 0])
         ]
-
-        normalized_sinr.extend(
-            [(k-1, (total_p - power_levels[device_id])/power_levels[device_id])
-             for device_id, k in enumerate(sim_history[slot]) if k != 0]
-        )
 
         # print curr_trans_results
         for device_id, curr_trans_result in enumerate(curr_trans_results):
             # 如果 curr_trans_result = True，则需要对这个包执行重传操作
             if curr_trans_result:
                 # 需要知道，这是第几次传输失败了
-                x = sim_history[slot][device_id]
+                # Don't forget to convert x into int type.
+                x = np.int(sim_history[slot, device_id, 0])
                 if x != max_trans:
                     # max_trans has not been reached. Execute backoff procedure
                     # The new slot index is the sum of current one and backoff length
                     while 1:
                         new_slot = int(np.random.exponential(scale=BACK_OFFS[x-1])) + 1 + slot
-                        if new_slot <= sim_duration-1:
+                        if new_slot < sim_duration:
                         # Take care that the selected new slot should not be out of range.
-                        # 这些重传规划在 simulation 之外的 packet 我们算他是失败呢还是成功呢？
-                            # Also we should note that selected new slot has not yet scheduled
-                            # for another retransmission
-                            # 到底有没有必要 专门找 not yet scheduled slot 呢？
-                            if sim_history[(new_slot, device_id)] == 0:
-                            # transmission trial should be incremented by 1
-                                sim_history[(new_slot, device_id)] = x+1
+                        # Scheduled packets out of simualtion is not considered in the following statisticals calculation stage
+                            # Also we should note that selected new slot has not yet scheduled for any others retransmission
+                            if sim_history[new_slot, device_id, 0] == 0:
+                                # copy the content of sim_history[slot, device_id, 0] to
+                                # sim_history[new_slot, device_id, 0]
+                                sim_history[new_slot, device_id] = sim_history[slot, device_id]
+                                # transmission trial should be incremented by 1
+                                sim_history[new_slot, device_id, 0] = x+1
+                                # Retransmission scheduling work is done. Break from while loop...
                                 break
+                        # 为什么这里 我要break呢？我觉得应该注释掉啊。。。
+                        # 我知道了 是因为到 simulation 的最后阶段，几乎已经不太可能在 simulation 时间范围内找到合适的 new_slot了
+                        # 所以 这种情况直接视为 已经在未来找到了合适的slot做 retransmission, 选择直接 break
                         else:
                             break
-                    # Do not forget to 清零 for this slot.
-                    sim_history[(slot, device_id)] = 0
+                    # Do not forget to reinitialize for this (slot, device) pair .
+                    sim_history[slot, device_id] = np.zeros(max_trans+1, dtype=np.float)
                 elif x == max_trans:
                     # The case where max_trans has been reached. Failure of this packet transmission
-                    sim_history[(slot, device_id)] = max_trans + 1
+                    sim_history[slot, device_id, 0] = max_trans + 1
 
+    # To convert the type of an array, use the .astype() method (preferred) or the type itself as a function.
+    # For example: z.astype(float)
+    trans_index_array = sim_history[warm_t:sim_duration, :, 0].reshape(device_nb*(sim_duration-warm_t))
+    trans_index_array = trans_index_array.astype(int)
+    # print "trans_index_array", [e for e in trans_index_array if e != 0]
     # 统计第 i 次传输出现的次数，并据此计算至少需要传输 i 次的频数，如果实验足够长，频数将趋近于概率
-    statistics = itemfreq(sim_history[warm_t:sim_duration, ::].reshape(1, device_nb*(sim_duration-warm_t))[0])[1:]
+    statistics = [[x, 0] for x in range(1, max_trans+2, 1)]
+
+    for entry_1 in itemfreq(trans_index_array)[1:]:
+        statistics[entry_1[0]-1][1] = entry_1[1]
+
+    # print "statistics", statistics
+
+    # Convert all received power level history into a one dimension array and calculate the total consumed energies
+    total_energies = sum(sim_history[warm_t:sim_duration, :, 1:].reshape(max_trans*device_nb*(sim_duration-warm_t)))
+
+    # 发现了两个bug: 一个是在处理failed packet的时候，错误地把所有的received power level 之和弄成了30
+    # 第二个bug是，程序之前并没有处理过simulation的最后一个slot, 结果统计的时候却把这一行尚处于buffered状态的packets都按成功处理了。。。
+    # (修改之后 packet loss rate 最后应该会提高一些。。。)
+    # for element in sim_history[warm_t:sim_duration]:
+    #     print [(hha[0], sum(hha[1:])) for hha in element]
+    # print sim_history[warm_t:sim_duration, :, :]
+    statistics_vector = [e[1] for e in statistics]
 
     vector_p = [0 for i in range(max_trans+1)]
 
-    total_transmission = sum([element[1] for element in statistics])
+    total_transmission = sum(statistics_vector)
+    # The following calculation requires that the last element is always the numerb of failed packets!
+    succ_transmission = sum(statistics_vector[0:-1])
 
     for element in statistics:
         ith_trans = element[0]
         satisfied_trans = sum([element[1] for element in statistics if element[0] >= ith_trans])
         vector_p[ith_trans - 1] = satisfied_trans*1.0/total_transmission
 
-    # Initialize the return probability vector with 0, the length of this vector is 1+MAX_ALLOWED_RETRANSMISSION_NB
-    # vector_p = [sum(statistics[i:])*1.0/sum(statistics) for i in range(len(statistics))]
+    # print "total_energies", total_energies
+    # print "succ_transmission", succ_transmission
 
-    empirical_cdfs = [[] for x in range(max_trans)]
-    for x in range(max_trans):
-        empirical_cdfs[x] = [element[1] for element in normalized_sinr if element[0] == x]
-
-    if output_statistics:
-        # 将统计结果写入到 文件里 默认不开启
-        cdf_csv_file = "empirical_cdf_{0}.csv".format(alpha)
-        cdf_start = 0.0
-        cdf_end = 6.0
-        with open(cdf_csv_file, 'w') as f_handler:
-            spamwriter = csv.writer(f_handler, delimiter=',')
-            for index, element in enumerate(empirical_cdfs):
-                ecdf = ECDF(element)
-                cdf = ecdf(np.arange(cdf_start, cdf_end, 0.02))
-                spamwriter.writerow(cdf)
-            spamwriter.writerow(transmissions_nb)
-
-    end_t = int(time())
-    time_elapsed = float(end_t-start_t)/60.0
-    statistics_vector = [e[1] for e in statistics]
+    energy_efficiency = total_energies/succ_transmission
+    statistics_vector.append(energy_efficiency)
     # calculate the throughput: amount of packets succesfully delivered per slot
     statistics_vector.extend(vector_p)
+    # Get end time and calculate elapsed time
+    end_t = int(time())
+    time_elapsed = float(end_t-start_t)/60.0
     print "Time:", int(time_elapsed), "Alpha:", alpha, "Seed:", seed, "Result:", statistics_vector
     return alpha, list(statistics), statistics_vector
 
@@ -270,9 +278,10 @@ if __name__ == "__main__":
     logs_directory = 'logs'
     SIM_CONFIG_DIR = 'sim_configs'
     # SIM_PART = 'fading_shadowing'
+    # SIM_PART = 'shadowing'
     SIM_PART = 'perfect'
 
-    SIM_CONFIG_FILE = 'case_K=5_l=1_m=1_threshold=3dB.json'
+    SIM_CONFIG_FILE = 'case_K=5_l=1_m=2_threshold=3dB.json'
     # The simulation result will be logged into files of type CSV, in folder logs.
     # First check the existence of this folder and creat it if necessary.
     if not os.path.exists(logs_directory):
--- perfect_no_fading/case1_3dB/plot_generation_case1.py
+++ perfect_no_fading/case1_3dB/plot_generation_case1.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import matplotlib.ticker as ticker
+import numpy as np
+import scipy.stats as st
+import pandas as pd
+import glob
+
 params = {
     'legend.fontsize': 25,
     'xtick.labelsize': 20,
@@ -15,95 +20,121 @@ params = {
 }
 plt.rcParams.update(params)
 
+LOG_DIR = 'logs'
+SUB_DIR = 'perfect'
 DATA_FOLDED = '.'
-plt.figure(1, figsize=(15, 12))
-plt.title("The packet loss rate evolution with SINR threshold = 3dB")
+FIGSIZE = (15, 12)
 
-plt.subplot(1, 1, 1)
-# The case for threshold 3.0 dB
+A_P_DECREMENT ="analytical, power decrement"
+A_P_IDENTIC = "analytical, identical power"
+A_P_INCREMENT = "analytical, power increment"
+S_P_IDENTIC = "simulation, identical power"
+S_P_INCREMENT = "simulation, power increment"
+S_P_DECREMENT = "simulation, power decrement"
+
+X_START = 0.2
+X_END = 1.01
+X_STEP = 0.1
+Y_START = 0.0
+Y_END = 1.01
+Y_STEP = 0.1
+
+def sim_data_process(sinr_thrsld, l, m, sigma_shadowing, backoff):
+    SUBSUB_DIR = "backoff_{0}".format(backoff)
+    CASE_DIR = 'case_{0}dB'.format(sinr_thrsld)
+    POWER_DIR = "l_{0}_m_{1}_sigma_s_{2}".format(l, m, sigma_shadowing)
+    logs_dir = glob.glob(os.path.join("..", "..", LOG_DIR, SUB_DIR, CASE_DIR, SUBSUB_DIR, POWER_DIR, "*.csv"))
+    sim_intensity = []
+    sim_plr =  []
+    sim_thrpt = []
+    for csv_file in logs_dir:
+        csv_df = pd.read_csv(csv_file, sep=',', header=None)
+        plr = csv_df.values[:, -2]
+        alpha = csv_df.values[:, -1][0]
+        sim_intensity.append(alpha)
+        plt = (np.sum(plr) - np.max(plr) - np.min(plr))/(len(plr)-2.0)
+        sim_plr.append(plt)
+        sim_thrpt.append(alpha*(1-plt))
+    return sim_intensity, sim_plr, sim_thrpt
+
+def analytic_data_process(f_name):
+    with open(f_name, 'r') as ana_f_handler:
+        ana_csv = list(csv.reader(ana_f_handler))
+        ana_intensity = [float(element[-1]) for element in ana_csv]
+        ana_plr = [float(element[-2]) for element in ana_csv]
+        ana_thrpt = [float(element[-1])*(1-float(element[-2])) for element in ana_csv]
+    return ana_intensity, ana_plr, ana_thrpt
+
+
+sim_no_intensity, sim_no_plr, sim_no_thrpt = sim_data_process(3, 1, 1, 0, 50)
+sim_more_intensity, sim_more_plr, sim_more_thrpt = sim_data_process(3, 2, 1, 0, 50)
+sim_less_intensity, sim_less_plr, sim_less_thrpt = sim_data_process(3, 1, 2, 0, 50)
+
 ana_result_f_no = os.path.join(DATA_FOLDED, "analytical_result_threshold=3.0_l=1_m=1.csv")
 ana_result_f_more = os.path.join(DATA_FOLDED, "analytical_result_threshold=3.0_l=2_m=1.csv")
 ana_result_f_less = os.path.join(DATA_FOLDED, "analytical_result_threshold=3.0_l=1_m=2.csv")
 
-# sim_result_f_no = os.path.join(DATA_FOLDED, "simd=50000_N=500_threshold=3.0_l=1_m=1_backoff=100.csv")
+ana_no_intensity, ana_no_plr, ana_no_thrpt = analytic_data_process(ana_result_f_no)
+ana_more_intensity, ana_more_plr, ana_more_thrpt = analytic_data_process(ana_result_f_more)
+ana_less_intensity, ana_less_plr, ana_less_thrpt = analytic_data_process(ana_result_f_less)
 
-with open(ana_result_f_no, 'r') as ana_no_f_handler, \
-        open(ana_result_f_more, 'r') as ana_more_f_handler, \
-        open(ana_result_f_less, 'r') as ana_less_f_handler:
+# 准备就绪，开始画图
+fig = plt.figure(1, figsize = FIGSIZE)
+ax = fig.add_subplot(111)    # The big subplot
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
 
-    ana_more_csv = list(csv.reader(ana_more_f_handler))
-    ana_more_intensity = [float(element[-1]) for element in ana_more_csv]
-    ana_more_plr = [float(element[-2]) for element in ana_more_csv]
+# Turn off axis lines and ticks of the big subplot
+ax.spines['top'].set_color('none')
+ax.spines['bottom'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
 
-    ana_no_csv = list(csv.reader(ana_no_f_handler))
-    ana_no_intensity = [float(element[-1]) for element in ana_no_csv]
-    ana_no_plr = [float(element[-2]) for element in ana_no_csv]
+# Set common labels
+ax.set_xlabel('Fresh Packet Arrival Intensity')
+ax.set_ylabel('Packet Loss Rate')
 
-    ana_less_csv = list(csv.reader(ana_less_f_handler))
-    ana_less_intensity = [float(element[-1]) for element in ana_less_csv]
-    ana_less_plr = [float(element[-2]) for element in ana_less_csv]
+# 生成子图1
+ax1.plot(ana_no_intensity, ana_no_thrpt, color='b',  marker='', linestyle='-', linewidth=2, label=A_P_IDENTIC)
+ax1.plot(ana_more_intensity, ana_more_thrpt, color='g', marker='', linestyle='-.', linewidth=2, label=A_P_INCREMENT)
+ax1.plot(ana_less_intensity, ana_less_thrpt, color='r', marker='', linestyle='--', linewidth=2, label=A_P_DECREMENT)
+ax1.plot(sim_no_intensity, sim_no_thrpt, color='b', marker='*', linestyle="", linewidth=2, label=S_P_IDENTIC)
+ax1.plot(sim_more_intensity, sim_more_thrpt, color='g', marker='o', linestyle="", linewidth=2, label=S_P_INCREMENT)
+ax1.plot(sim_less_intensity, sim_less_thrpt, color='r', marker='^', linestyle="", linewidth=2, label=S_P_DECREMENT)
+# ax1.legend(loc='best', numpoints=2)
+ax1.set_yticks(np.arange(Y_START, Y_END, Y_STEP))
+ax1.set_xticks(np.arange(X_START, X_END, X_STEP))
+ax1.axis([X_START, X_END, Y_START, Y_END])
+ax1.set_title("SINR Threshold 3dB")
+ax1.grid()
 
-sim_result_f_no = os.path.join(DATA_FOLDED, "simd=50000_N=500_threshold=3.0_l=1_m=1_backoff=150_start=0.2_end=1.05_simstep=0.02_093120.csv")
-
-with open(sim_result_f_no, 'r') as sim_no_f_handler:
-    sim_no_csv = list(csv.reader(sim_no_f_handler))
-    sim_no_intensity = [float(element[-1]) for element in sim_no_csv]
-    sim_no_plr = [float(element[-2]) for element in sim_no_csv]
-
-sim_result_f_more = os.path.join(
-    DATA_FOLDED, "simd=50000_N=500_threshold=3.0_l=2_m=1_backoff=150_start=0.2_end=0.8_simstep=0.02_045109.csv"
-)
-with open(sim_result_f_more, 'r') as sim_more_f_handler:
-    sim_more_csv = list(csv.reader(sim_more_f_handler))
-    sim_more_intensity = [float(element[-1]) for element in sim_more_csv]
-    sim_more_plr = [float(element[-2]) for element in sim_more_csv]
-    # plt.xticks(np.arange(0, max(ana_with_intensity), 0.1))
-
-
-sim_result_f_less = os.path.join(
-    DATA_FOLDED,
-    "simd=50000_N=400_threshold=3.0_l=1_m=2_backoff=150_start=0.24_end=0.9_simstep=0.02_065325.csv"
-)
-with open(sim_result_f_less, 'r') as sim_less_f_handler:
-    sim_less_csv = list(csv.reader(sim_less_f_handler))
-    sim_less_intensity = [float(element[-1]) for element in sim_less_csv]
-    sim_less_plr = [float(element[-2]) for element in sim_less_csv]
-
-
-#     plt.xlabel("The fresh packet arrival intensity")
-#     plt.ylabel("The packet loss rate")
-#     plt.plot(ana_less_intensity, ana_less_plr, 'r-', label="analytical result with power decrement")
-#     plt.plot(ana_no_intensity, ana_no_plr, 'b-', label="analytical result case with identical power")
-#     plt.plot(ana_more_intensity, ana_more_plr, 'g-', label="analytical result with power increment")
-#     plt.plot(sim_no_intensity, sim_no_plr, 'b*', label="simulation result with identical power")
-#     plt.plot(sim_more_intensity, sim_more_plr, 'go', label="simulation result with power increment")
-#     plt.plot(sim_less_intensity, sim_less_plr, 'r+', label="simulation result with power decrement")
+# 生成子图2
+# ax2.semilogy(ana_no_intensity, ana_no_plr, color='b',  marker='', linestyle='-', linewidth=2, label=A_P_IDENTIC)
+# ax2.semilogy(ana_more_intensity, ana_more_plr, color='g', marker='', linestyle='-.', linewidth=2, label=A_P_INCREMENT)
+# ax2.semilogy(ana_less_intensity, ana_less_plr, color='r', marker='', linestyle='--', linewidth=2, label=A_P_DECREMENT)
+# ax2.semilogy(sim_no_intensity, sim_no_plr, color='b', marker='*', linestyle="", linewidth=2, label=S_P_IDENTIC)
+# ax2.semilogy(sim_more_intensity, sim_more_plr, color='g', marker='o', linestyle="", linewidth=2, label=S_P_INCREMENT)
+# ax2.semilogy(sim_less_intensity, sim_less_plr, color='r', marker='^', linestyle="", linewidth=2, label=S_P_DECREMENT)
 #
-#
-#
-#     plt.xlabel("The fresh packe arrival intensity")
-#     plt.ylabel("The packet loss rate")
-#     plt.legend(loc='best', numpoints=2)
-#     plt.axis([0, 1.05, 0, 1.05])
-#     plt.grid()
-#
-# plt.subplot(2, 1, 2)
-plt.grid()
+# ax2.legend(loc='best', numpoints=2)
+# ax2.set_xticks(np.arange(X_START, X_END, X_STEP))
+# ax2.axis([X_START, X_END, Y_START, Y_END])
+# ax2.axis([X_START, X_END, Y_START, Y_END])
+# ax2.grid()
 
-plt.semilogy(ana_less_intensity, ana_less_plr, 'r-', label="analytical result with power decrement")
-plt.semilogy(ana_no_intensity, ana_no_plr, 'b-', label="analytical result case with identical power")
-plt.semilogy(ana_more_intensity, ana_more_plr, 'g-', label="analytical result with power increment")
-plt.semilogy(sim_no_intensity, sim_no_plr, 'b*', label="simulation result with  identical power")
-plt.semilogy(sim_more_intensity, sim_more_plr, 'go', label="simulation result with power increment")
-plt.semilogy(sim_less_intensity, sim_less_plr, 'r+', label="simulation result with power decrement")
+ax2.semilogy(ana_no_intensity, ana_no_plr, color='b',  marker='', linestyle='-', linewidth=2, label=A_P_IDENTIC)
+ax2.semilogy(ana_more_intensity, ana_more_plr, color='g', marker='', linestyle='-.', linewidth=2, label=A_P_INCREMENT)
+ax2.semilogy(ana_less_intensity, ana_less_plr, color='r', marker='', linestyle='--', linewidth=2, label=A_P_DECREMENT)
+ax2.semilogy(sim_no_intensity, sim_no_plr, color='b', marker='*', linestyle="", linewidth=2, label=S_P_IDENTIC)
+ax2.semilogy(sim_more_intensity, sim_more_plr, color='g', marker='o', linestyle="", linewidth=2, label=S_P_INCREMENT)
+ax2.semilogy(sim_less_intensity, sim_less_plr, color='r', marker='^', linestyle="", linewidth=2, label=S_P_DECREMENT)
 
+ax2.legend(loc='best', numpoints=2)
+ax2.set_xticks(np.arange(X_START, X_END, X_STEP))
+ax2.axis([X_START, X_END, Y_START, Y_END])
+ax2.axis([X_START, X_END, Y_START, Y_END])
+ax2.grid()
 
-plt.legend(loc='best', numpoints=2)
-plt.ylabel("The packet loss rate")
-plt.xlabel("The fresh packe arrival intensity")
-
-plt.axis([0, 2.05, 0, 1.05])
-
-
-plt.savefig('logarithme_case1.eps', format='eps', dpi=300)
+plt.savefig('throughput_case1.eps', format='eps', dpi=300)
 plt.show()
